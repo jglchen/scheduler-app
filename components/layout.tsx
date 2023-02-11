377761de1c66a2162bd7d6ed8806eb94c4b861b2
John Chen <jglchen@gmail.com>
2023-02-11 17:38:46 +0800

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,6 +6,15 @@ export default function Layout({ children}: {children: JSX.Element}) {
            <Head>
              <title>Appointment Scheduler Application</title>
              <link rel="icon" href="/favicon.ico" />
+             <meta
+              name="description"
+              content="Appointment Scheduler Application"
+              />
+             <meta name="og:title" content="Appointment Scheduler Application" />
+             <meta
+              property="og:description"
+              content="Appointment Scheduler Application"
+              />
            </Head>
            <main>
               {children}
